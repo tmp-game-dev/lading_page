475305313d373a489a76a796c1a5748bd0e6b779
Rabeles <9david@seznam.cz>
2023-01-15 18:48:08 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -19,36 +19,10 @@ const Image = styled.img`
   transition: opacity 500ms, transform 500ms;
 `;
 
-const Image2 = styled(Image)`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  display: block;
-  transition: opacity 500ms, transform 500ms;
-`;
-
-
-const NewContentDiv = styled.div`
-  position: absolute;
-  width: 100%;
-  background-color: red;
-  overflow: scroll;
-  top: 200vh;
-  left: 0;
-  height: 10vh;
-`;
-
 function App() {
   const ref = useRef<any>(null);
   const [bottom, setBottom] = useState<boolean>(false);
   const [transition, setTransition] = useState<boolean>(false);
-  const [zommie, setZommie] = useState<boolean>(false);
-  const [opacity, setOpacity] = useState(0);
-  const [scale, setScale] = useState(2.0);
-  const [bottomPage, setBottomPage] = useState<boolean>(false);
-
 
   const handleScroll = () => {
     if (ref.current) {
@@ -67,58 +41,21 @@ function App() {
   };
 
   const handleScrollTop = () => {
-    if (opacity > 0 && ref.current) {
-      setOpacity(opacity - 0.2);
-    }
-    if (opacity <= 0 && ref.current) {
+
+    if (ref.current) {
+      // eslint-disable-next-line no-unused-vars
       const { scrollTop, clientHeight, scrollHeight } = ref.current;
       if (scrollTop === 0) {
         setTransition(true);
-        setZommie(false);
         setTimeout(() => {
           setBottom(false);
           setTransition(false);
           if (ref.current !== null) {
-            ref.current.scrollTo(0, clientHeight - 550);
+            ref.current.scrollTo(0, clientHeight - 150);
           }
         }, 500);
       }
 
-      if (scrollTop + clientHeight === scrollHeight) {
-        setZommie(true);
-      }
-    }
-  };
-
-  const handleWheel = (e: any) => {
-    if (ref.current) {
-      const { scrollTop, clientHeight, scrollHeight } = ref.current;
-
-      if (opacity === 1) {
-        setBottomPage(true);
-      }
-      if (opacity > 0) {
-        if (e.deltaY < 0 && scrollTop > 0) {
-          console.log("UP");
-          setOpacity(opacity - 0.2);
-          setScale(0.2 + scale);
-        }
-      }
-      if (opacity < 1) {
-        if (e.deltaY > 0) {
-          console.log("Dolůůů");
-        }
-        if (
-          e.deltaY > 0 &&
-          scrollTop + clientHeight === scrollHeight &&
-          !bottomPage
-        ) {
-          console.log("down");
-          setOpacity(0.2 + opacity);
-          setScale(scale - 0.2);
-        }
-        setBottomPage(false);
-      }
     }
   };
 
@@ -126,11 +63,9 @@ function App() {
     // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
       {bottom ? (
-        <>
           <Background
             ref={ref}
             onScroll={handleScrollTop}
-            onWheel={handleWheel}
           >
             <Image
               src="/images/background/background2.png"
@@ -140,20 +75,7 @@ function App() {
                 transform: `scale(${transition ? 1.5 : 1})`,
               }}
             />
-            {zommie && (
-                <Image2
-                  src="/images/background/background3T.png"
-                  alt="image"
-                  style={{
-                    opacity,
-                    display: opacity < 0.2 ? "none" : "block",
-                    transform: `scale(${scale})`,
-                  }}
-                />
-            )}
           </Background>
-          <NewContentDiv>Hello</NewContentDiv>
-        </>
       ) : (
         <Background ref={ref} onScroll={handleScroll}>
           <Image
